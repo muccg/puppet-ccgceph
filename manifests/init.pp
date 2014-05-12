a4650dfb58e1a8fe5426818478ea17bb1ff9a1a4
ccgetckeeper <ccgetckeeper@swiftn7>
2014-05-12 12:00:07 +0800

    the mon setup seems to be working

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,25 +2,23 @@
 class ccgceph () inherits ccgceph::params {
     # we are doubling up monitor with osd for now (small rollout on the compute nodes)
 
-    ceph::conf {
+    class { 'ceph': 
         fsid => $ceph_fsid,
         mon_host => 'compute0.bpa.ccg,compute1.bpa.ccg,compute2.bpa.ccg',
-    };
-ceph::mon { $hostname: key => $ceph_mon_key }
-    ceph::key {'client.admin':
-        secret => ceph_admin_key,
-        caps_mon => '*',
-        caps_osd => '*',
-        inject => true
     }
-    cceph::key{'client.bootstrap-osd':
-        secret => $ceph_bootstrap_osd_key,
-        caps_mon => 'profile bootstrap-osd',
-        inject => true
-    }
-    ceph::osd{ 'discover' };
-    ceph::key{'client.bootstrap-osd':
-        keyring => '/var/lib/ceph/bootstrap-osd/ceph.keyring',
-        secret => $ceph_bootstrap_osd_key,
-    }
+
+    ceph::mon { $hostname: key => $ceph_mon_key }
+
+    #ceph::key {'client.admin':
+    #    secret => $ceph_admin_key,
+    #    cap_mon => '*',
+    #    cap_osd => '*',
+    #    inject => true
+    #}
+    # ceph::key{'client.bootstrap-osd':
+    #     secret => $ceph_bootstrap_osd_key,
+    #     cap_mon => 'profile bootstrap-osd',
+    #     inject => true
+    # }
+    # ceph::osd{'discover':}
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -17,8 +17,9 @@ class ccgceph::params {
     ]
 
     # must remain constant over a ceph cluster (nothing to do with cephfs)
+    # NB these are samples, change these before going live... - GB
     $ceph_fsid = 'E8FEB982-B17B-43BF-8E07-34CAF47A2F96'
-    $ceph_admin_key = 'ZGFoTm9vMmllYjNSaU1vYTRpYTRPaHJpNUFmYWVkYWU='
-    $ceph_mon_key = 'bWFoSDZpZWRvbzV0b2hsZXcwaGVldmFlZ2hhZThpdWo='
-    $ceph_bootstrap_osd_key = 'cmFpN2dhaENoYWlwYWk1a2Vla2llbGVlNG1laTlub2g='
+    $ceph_admin_key = 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ=='
+    $ceph_mon_key = 'AQDesGZSsC7KJBAAw+W/Z4eGSQGAIbxWjxjvfw=='
+    $ceph_bootstrap_osd_key = 'AQABsWZSgEDmJhAAkAGSOOAJwrMHrM5Pz5On1A=='
 }
