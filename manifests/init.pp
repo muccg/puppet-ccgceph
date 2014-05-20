a974477a2e0468d40c4d32d46e2e43034dc2b345
ccgetckeeper <ccgetckeeper@swiftn7>
2014-05-20 11:46:48 +0800

    mostly works

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,36 @@
 #
-class ccgceph () inherits ccgceph::params {
-    # we are doubling up monitor with osd for now (small rollout on the compute nodes)
-
-    class { 'ceph': 
-        fsid => $ceph_fsid,
-        mon_host => 'compute0.bpa.ccg,compute1.bpa.ccg,compute2.bpa.ccg',
+class ccgceph ($ceph_fsid, $mon_secret) inherits ccgceph::params {
+    # puppet-ceph needs us to set this global path
+    Exec {
+      path => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
     }
 
-    ceph::mon { $hostname: key => $ceph_mon_key }
+    # we are doubling up monitor with osd for now (small rollout on the compute nodes)
+    class { 'ceph::conf':
+      fsid => $ceph_fsid,
+      auth_type => 'cephx',
+      cluster_network => '10.0.1.0/24',
+      public_network => '10.0.2.0/24'
+    }
 
-    #ceph::key {'client.admin':
-    #    secret => $ceph_admin_key,
-    #    cap_mon => '*',
-    #    cap_osd => '*',
-    #    inject => true
-    #}
-    # ceph::key{'client.bootstrap-osd':
-    #     secret => $ceph_bootstrap_osd_key,
-    #     cap_mon => 'profile bootstrap-osd',
-    #     inject => true
-    # }
-    # ceph::osd{'discover':}
+    ceph::mon { $hostnum:
+      monitor_secret => $mon_secret,
+      mon_port       => 6789,
+      mon_addr       => $ipaddress_br100,
+    }
+
+    class { 'ceph::osd' :
+      public_address  => $ipaddress_br100,
+      cluster_address => $ipaddress_eth1,
+    }
+    ceph::osd::device { '/dev/sdc': }
+    ceph::osd::device { '/dev/sdd': }
+    ceph::osd::device { '/dev/sde': }
+    ceph::osd::device { '/dev/sdf': }
+    ceph::osd::device { '/dev/sdg': }
+    ceph::osd::device { '/dev/sdh': }
+    ceph::osd::device { '/dev/sdi': }
+    ceph::osd::device { '/dev/sdj': }
+    ceph::osd::device { '/dev/sdk': }
+    ceph::osd::device { '/dev/sdl': }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 #
-class ccgceph::params {
+class ccgceph::params () {
 
     case $::hostname {
       /^compute([\d+])$/: {
@@ -15,11 +15,4 @@ class ccgceph::params {
 
     $packages = [
     ]
-
-    # must remain constant over a ceph cluster (nothing to do with cephfs)
-    # NB these are samples, change these before going live... - GB
-    $ceph_fsid = 'E8FEB982-B17B-43BF-8E07-34CAF47A2F96'
-    $ceph_admin_key = 'AQCTg71RsNIHORAAW+O6FCMZWBjmVfMIPk3MhQ=='
-    $ceph_mon_key = 'AQDesGZSsC7KJBAAw+W/Z4eGSQGAIbxWjxjvfw=='
-    $ceph_bootstrap_osd_key = 'AQABsWZSgEDmJhAAkAGSOOAJwrMHrM5Pz5On1A=='
 }
