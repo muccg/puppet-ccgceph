5081a6e6ceb330a7b9f55c50ed35e497e85125cc
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2014-05-20 14:53:21 +0800

    set things up for ceph-deploy

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,36 +1,27 @@
 #
 class ccgceph ($ceph_fsid, $mon_secret) inherits ccgceph::params {
-    # puppet-ceph needs us to set this global path
-    Exec {
-      path => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
+    include repo::ceph
+
+    package { $ccgceph::absent_packages:
+        ensure  => absent,
+        require => Class['openstack']
     }
 
-    # we are doubling up monitor with osd for now (small rollout on the compute nodes)
-    class { 'ceph::conf':
-      fsid => $ceph_fsid,
-      auth_type => 'cephx',
-      cluster_network => '10.0.1.0/24',
-      public_network => '10.0.2.0/24'
+    package { $ccgceph::packages:
+        ensure  => present,
+        require => Class['openstack']
     }
 
-    ceph::mon { $hostnum:
-      monitor_secret => $mon_secret,
-      mon_port       => 6789,
-      mon_addr       => $ipaddress_br100,
+    class { 'sudo':
+        purge               => false,
+        config_file_replace => false,
+    } ->
+    user { "ceph":
+        ensure => present,
+        comment => "Ceph",
+    } ->
+    sudo::conf { 'ceph':
+        priority => 10,
+        content  => 'ceph ALL = (root) NOPASSWD:ALL',
     }
-
-    class { 'ceph::osd' :
-      public_address  => $ipaddress_br100,
-      cluster_address => $ipaddress_eth1,
-    }
-    ceph::osd::device { '/dev/sdc': }
-    ceph::osd::device { '/dev/sdd': }
-    ceph::osd::device { '/dev/sde': }
-    ceph::osd::device { '/dev/sdf': }
-    ceph::osd::device { '/dev/sdg': }
-    ceph::osd::device { '/dev/sdh': }
-    ceph::osd::device { '/dev/sdi': }
-    ceph::osd::device { '/dev/sdj': }
-    ceph::osd::device { '/dev/sdk': }
-    ceph::osd::device { '/dev/sdl': }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -14,5 +14,6 @@ class ccgceph::params () {
     ]
 
     $packages = [
+        'ceph-deploy'
     ]
 }
