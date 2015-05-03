090ae262f9999a2908f7b65264353007176654f3
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-05-03 16:25:14 +0800

    minor tidy up

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name         'muccg-ccgceph'
-version      '1.2.1'
+version      '1.2.2'
 
 author       'Centre for Comparative Genomics'
 license      'GNU GPL Version 3'
@@ -7,3 +7,5 @@ project_page 'https://github.com/muccg/puppet-ccgceph'
 source       'git://github.com/muccg/puppet-ccgceph'
 summary      'Puppet module for CCG OpenStack ceph nodes'
 description  'Installs and manages OpenStack dependencies for CCG ceph nodes.'
+
+dependency 'saz/sudo', '>=3.0.9 <3.1.0'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 #
 class ccgceph ($ceph_fsid, $mon_secret) inherits ccgceph::params {
-    # include repo::ceph
 
     package { $ccgceph::absent_packages:
         ensure  => absent
@@ -14,19 +13,23 @@ class ccgceph ($ceph_fsid, $mon_secret) inherits ccgceph::params {
         purge               => false,
         config_file_replace => false,
     } ->
+
     user { "ceph":
-        ensure => present,
+        ensure     => present,
         managehome => true,
-        shell => '/bin/bash',
-        comment => "Ceph",
+        shell      => '/bin/bash',
+        comment    => "Ceph",
     } ->
+
     sudo::conf { 'ceph':
         priority => 10,
         content  => 'ceph ALL = (root) NOPASSWD:ALL',
-    } -> 
+    } ->
+
     file {'/home/ceph/.ssh/':
         ensure => "directory"
-    } -> 
+    } ->
+
     file {'/home/ceph/.ssh/authorized_keys':
       content => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDR5riAD3/sNQoAJce6LQo4HZQzKZzGPH/8SYKpyx7qQpRK0ivOpX4tUT4XuH+5z7jXG8sIijrC7/MmWGjwxPSNnEGxrakCmWKWWvbG721PH9+xyHsVz6onqSH+CIH2AJFFOZOizGjaXZles9/hKY47CPqmhtBSNbRojYArQTaWRFQU02mA7mKIW4oVMFsQjocs/6Q8hqDOAwzMlTbIhRyVAf7eD/A2mFImOK/zR9Zvw5AVro5oi1wI9vg2ynzHD3X3CTlH5IA3L4wkToMTJI4SkV+TRklGmZD5sTAHzg+NG18SH19Q8gK+4Kl0m9bVXhK1PUzPOCSpwRaTvpGqjIAj ceph@compute0"
     }
