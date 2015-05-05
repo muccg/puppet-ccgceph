e32b7eec7842466151222665d6b21da1e7883639
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-05-05 12:09:07 +0800

    remove legacy, class seems redundant

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,7 @@
 #
 # TODO
-#  - why is hostnum declared?
-#  - why are params passed through? compute?
 #  - A lot of code to install one package??
-class ccgceph ($ceph_fsid, $mon_secret) {
-
-
-    case $::hostname {
-      /^compute(\d+)$/: {
-        $hostnum = $1
-      }
-      # match docker test node name
-      /^computenode$/: {
-        $hostnum = $1
-      }
-      /^ceph(\d+)$/: {
-        $hostnum = $1
-      }
-      default: {
-        fail("${::hostname} does not match expected hostname pattern")
-      }
-    }
+class ccgceph {
 
     $absent_packages = [
     ]
