4a5ae36a51941bdfde3457f3f5e3d3c1d29bfa7b
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-05-05 12:02:49 +0800

    simplify

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name         'muccg-ccgceph'
-version      '1.2.2'
+version      '1.3.0'
 
 author       'Centre for Comparative Genomics'
 license      'GNU GPL Version 3'
@@ -7,5 +7,3 @@ project_page 'https://github.com/muccg/puppet-ccgceph'
 source       'git://github.com/muccg/puppet-ccgceph'
 summary      'Puppet module for CCG OpenStack ceph nodes'
 description  'Installs and manages OpenStack dependencies for CCG ceph nodes.'
-
-dependency 'saz/sudo', '>=3.0.9 <3.1.0'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,33 @@
 #
-class ccgceph ($ceph_fsid, $mon_secret) inherits ccgceph::params {
+# TODO
+#  - why is hostnum declared?
+#  - why are params passed through? compute?
+#  - A lot of code to install one package??
+class ccgceph ($ceph_fsid, $mon_secret) {
+
+
+    case $::hostname {
+      /^compute(\d+)$/: {
+        $hostnum = $1
+      }
+      # match docker test node name
+      /^computenode$/: {
+        $hostnum = $1
+      }
+      /^ceph(\d+)$/: {
+        $hostnum = $1
+      }
+      default: {
+        fail("${::hostname} does not match expected hostname pattern")
+      }
+    }
+
+    $absent_packages = [
+    ]
+
+    $packages = [
+        'ceph-deploy'
+    ]
 
     package { $ccgceph::absent_packages:
         ensure  => absent
@@ -7,30 +35,5 @@ class ccgceph ($ceph_fsid, $mon_secret) inherits ccgceph::params {
 
     package { $ccgceph::packages:
         ensure  => present
-    }
-
-    class { 'sudo':
-        purge               => false,
-        config_file_replace => false,
-    } ->
-
-    user { "ceph":
-        ensure     => present,
-        managehome => true,
-        shell      => '/bin/bash',
-        comment    => "Ceph",
-    } ->
-
-    sudo::conf { 'ceph':
-        priority => 10,
-        content  => 'ceph ALL = (root) NOPASSWD:ALL',
-    } ->
-
-    file {'/home/ceph/.ssh/':
-        ensure => "directory"
-    } ->
-
-    file {'/home/ceph/.ssh/authorized_keys':
-      content => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDR5riAD3/sNQoAJce6LQo4HZQzKZzGPH/8SYKpyx7qQpRK0ivOpX4tUT4XuH+5z7jXG8sIijrC7/MmWGjwxPSNnEGxrakCmWKWWvbG721PH9+xyHsVz6onqSH+CIH2AJFFOZOizGjaXZles9/hKY47CPqmhtBSNbRojYArQTaWRFQU02mA7mKIW4oVMFsQjocs/6Q8hqDOAwzMlTbIhRyVAf7eD/A2mFImOK/zR9Zvw5AVro5oi1wI9vg2ynzHD3X3CTlH5IA3L4wkToMTJI4SkV+TRklGmZD5sTAHzg+NG18SH19Q8gK+4Kl0m9bVXhK1PUzPOCSpwRaTvpGqjIAj ceph@compute0"
     }
 }
